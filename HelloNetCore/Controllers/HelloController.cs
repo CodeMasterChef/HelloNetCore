208efdef24b21141aa9755d9fb1a0dd15e382e53
Ninh Le <levanninh0101@gmail.com>
2018-06-09 10:50:34 +0700

    first commit

---
+++ HelloNetCore/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloNetCore.Controllers
+{
+    [Route("/")]
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World!";
+        }
+
+    }
+}
